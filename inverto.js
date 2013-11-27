ce9eb6277cf6be3239303456101bf283cb6890e9
jorgenbs <jorgenbs@gmail.com>
2013-11-27 16:03:14 +0100

    working

--- inverto.js
+++ inverto.js
@@ -1,38 +1,33 @@
-'use strict';
-window.inverto = window.inverto || function (selector) {
-  var className = 'inverto90A2' 
-      , css     = '.'+className+' {-webkit-filter: invert(100%);-moz-filter: invert(100%);-o-filter: invert(100%);-ms-filter: invert(100%);filter: invert(100%);filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'invert\'><feColorMatrix in=\'SourceGraphic\' type=\'matrix\' values=\'-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0\'/></filter></svg>#invert"); }'
-      , head    = document.getElementsByTagName('head')[0]
-      , style   = document.createElement('style')
-      , el      = document.querySelectorAll(selector)
-      , styles  = document.styleSheets
-      , addCss  = true;
+(function () {
+  'use strict';
+  window.inverto = function() {
+    var css = 'html {-webkit-filter: invert(100%);' +
+          '-moz-filter: invert(100%);' +
+          '-o-filter: invert(100%);' +
+          '-ms-filter: invert(100%); ' +
+          'filter: invert(100%);' +
+          'filter: url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'invert\'><feColorMatrix in=\'SourceGraphic\' type=\'matrix\' values=\'-1 0 0 0 1 0 -1 0 0 1 0 0 -1 0 1 0 0 0 1 0\'/></filter></svg>#invert"); }'
+      , head  = document.getElementsByTagName('head')[0]
+      , proto = window.inverto.prototype;
 
-  if (styles.length != 0) {
-    var appended = styles[styles.length-1];
-    if (appended && appended.cssRules.length === 1) {
-      if (appended.cssRules[0].selectorText.toUpperCase() === ('.'+className).toUpperCase()) {
-        addCss = false;
-      }
+    if (!proto.isApplied) {
+      var style = proto.style = document.createElement('style');
+    
+      style.type = 'text/css';
+      if (style.styleSheet) {
+        style.styleSheet.cssText = css;
+      } 
+      else style.appendChild(document.createTextNode(css));
+
+      head.appendChild(style);
+      proto.isApplied = true;
+      return true;
     }
-  }
-
-  if (addCss) {
-    style.type = 'text/css';
-    if (style.styleSheet) {
-      style.styleSheet.cssText = css;
-    } 
-    else style.appendChild(document.createTextNode(css))
-
-    head.appendChild(style);
-  }
-
-  if (el.length > 0) {
-    for (var i=0; i<el.length; i++)
-      var cl = el[i].classList;
-      var index = cl.indexOf
-      if (!cl.contains(className)) cl.add(className);
-      else cl.remove(className);
-  }
-  return el;
-}
+    else {
+      head.removeChild(proto.style);
+      proto.isApplied = false;
+    }
+    return false;
+  };
+  window.inverto.prototype.isApplied = false;
+})()
